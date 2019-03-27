d375aca62fe9bbf732318d73267ed3b360f65c24
unknown <merchan.ber.mau@gmail.com>
2019-03-26 20:39:37 -0500

    creando componente para el chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      message:'',
+      messages : [
+        {id:0,text:"Hola1"},
+        {id:1,text:"Hola2"},
+        {id:2,text:"Hola3"},
+      ]
+    }
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    const list = this.state.messages;
+    const newMessage={
+      id: this.state.messages.length,
+      text:this.state.message
+    }
+    list.push(newMessage);
+    this.setState({messages:list});
+    this.setState({message:''});
+  }
+  updateMessage(e){
+    this.setState({message:e.target.value});
+    console.log(this.state.message);
+  }
   render() {
+    const {messages}= this.state;
+    const messagesList = messages.map(message=>{
+      return <li key={message.id}>{message.text}</li>
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <ul>
+        {messagesList}
+        </ul>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input value={this.state.message} type="text" onChange={this.updateMessage.bind(this)}/>
+          <button>send</button>
+        </form>
       </div>
+     
     );
   }
 }
